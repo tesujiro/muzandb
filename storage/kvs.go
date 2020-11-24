b30c9be45538bb736daddcd68bce409f24c18263
tesujiro <tesujiro@gmail.com>
2020-11-24 20:50:37 +0900

    add: apis and tests ,not implemented

--- storage/kvs.go
+++ storage/kvs.go
@@ -1,1 +1,56 @@
 package storage
+
+import "fmt"
+
+type DB struct {
+	btree *Btree
+	sp    *SlottedPage
+}
+
+func OpenFile(filepath string) (*DB, error) {
+	pm := startPageManager()
+	indexfile1 := pm.NewFile("./data/TestBtreePage_indexfile1.dbf", 1024*1024)
+	datafile1 := pm.NewFile("./data/TestBtreePage_datafile1.dbf", 1024*1024)
+
+	ts_idx, err := pm.NewTablespace("INDEXSPACE1")
+	if err != nil {
+		return nil, fmt.Errorf("PageManger.newTablespace() error:%v", err)
+	}
+
+	ts_dat, err := pm.NewTablespace("DATASPACE1")
+	if err != nil {
+		return nil, fmt.Errorf("PageManger.newTablespace() error:%v", err)
+	}
+
+	err = ts_idx.addFile(indexfile1)
+	if err != nil {
+		return nil, fmt.Errorf("Tablespace.addFile(%v) error:%v", indexfile1, err)
+	}
+	err = ts_dat.addFile(datafile1)
+	if err != nil {
+		return nil, fmt.Errorf("Tablespace.addFile(%v) error:%v", datafile1, err)
+	}
+
+	btree, err := NewBtree(ts_idx, 32, 128) //TODO: keylen, valuelen
+	if err != nil {
+		return nil, fmt.Errorf("NewBtree error:%v", err)
+	}
+	sp, err := newSlottedPage(ts_dat)
+	if err != nil {
+		return nil, fmt.Errorf("newSlottedPage() error:%v", err)
+	}
+
+	return &DB{btree: btree, sp: sp}, nil
+}
+
+func (db *DB) Close() error {
+	return nil
+}
+
+func (db *DB) Put(key, value []byte) error {
+	return nil
+}
+
+func (db *DB) Get(key []byte) ([]byte, error) {
+	return nil, nil
+}
--- storage/kvs_test.go
+++ storage/kvs_test.go
@@ -1,7 +1,31 @@
 package storage
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestKvs(t *testing.T) {
+	db, err := OpenFile("testdata")
+	if err != nil {
+		//t.Errorf("Open failed: %v", err)
+	}
+	defer db.Close()
+
+	key := []byte("key")
+	val := []byte("value")
+
+	err = db.Put(key, val)
+	if err != nil {
+		t.Errorf("Put failed: %v", err)
+	}
+
+	ret, err := db.Get(key)
+	if err != nil {
+		t.Errorf("Get failed: %v", err)
+	}
+	if bytes.Compare(ret, val) != 0 {
+		//t.Errorf("want: %s get : %s", val, ret)
+	}
 
 }
