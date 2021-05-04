c94a7e7a370be74187c60b77bd9aae9013c592d6
tesujiro <tesujiro@gmail.com>
2021-05-04 15:01:58 +0900

    refact: devide error package

---
+++ errors/errors.go
@@ -0,0 +1,12 @@
+package storage
+
+import "errors"
+
+var (
+	AlreadyDeletedError = errors.New("Already deleted.")
+	NoSpaceError        = errors.New("The page does not have enough space.")
+	NoSuchSlotError     = errors.New("The page does not have the slot.")
+	DuplicateKeyError   = errors.New("The key is duplicate.")
+	NoKeyError          = errors.New("The key does not exist.")
+	NodeOverflowError   = errors.New("The node has overflowed.")
+)
